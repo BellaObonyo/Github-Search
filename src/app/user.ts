792675d100ad9c8ed93ee4c7706b21dd65a2ee05
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-07-25 17:04:25 +0300

    initialised user.ts file

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,2 +1,9 @@
 export class User {
-}
+    constructor(
+        public login:string,
+        public avatar_url:any,
+        public followers:any,
+        public following:any,
+        public public_repos:any,
+        ){}
+    }
