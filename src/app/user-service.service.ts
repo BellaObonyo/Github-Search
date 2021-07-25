9f6e1da20c8e7d81e8d0307dad96cf329909384a
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-07-25 17:17:20 +0300

    updated user -service.ts file

--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,9 +1,72 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { User } from './user';
+import { Repo } from './repo';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserServiceService {
+export class UserService {
 
-  constructor() { }
+  Users!: User;
+Repos:Repo[]=[]
+
+constructor (private http:HttpClient) { }
+searchGits(searchTerm:string){
+  interface userInterface{
+    login:string,
+    avatar_url:any,
+    followers:any,
+    following:any,
+    public_repos: any,
+  }
+  let urlUser = "https://api.github.com/users/"+searchTerm;
+  
+  let promise = new Promise((resolve,reject)=>{
+    this.http.get<userInterface>(urlUser).toPromise().then(
+      (result)=>{
+        
+       this.Users=result
+        console.log(this.Users);
+        
+        resolve(result)
+      },
+      (error)=>{
+        console.log()
+        reject()
+      }
+    )
+  })
+  return promise
+}
+searchRepos(searchTerm:string){
+  interface repoInterface{
+    name:string,
+    description:string,
+    html_url:any,
+  }
+
+  let urlUser = "https://api.github.com/users/"+ searchTerm +"/repos";
+
+  let promise = new Promise((resolve,reject)=>{
+    this.http.get<repoInterface[]>(urlUser).toPromise().then(
+      (results)=>{
+
+        this.Repos= [];
+        for (let i=0; i<results.length;i++){
+          let repo = new Repo(results[i].name,results[i].description,results[i].html_url)
+          this.Repos.push(repo);
+        }
+        console.log(results);
+        resolve(results)
+      },
+      (error)=>{
+        console.log()
+        reject()
+      }
+    )
+  })
+  return promise
+}
 }
