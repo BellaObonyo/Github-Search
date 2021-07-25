cd174556b1986398a111dd316b03734d8846b522
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-07-25 16:47:57 +0300

    initialized my repository

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GithubComponent } from './github/github.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GithubComponent
   ],
   imports: [
     BrowserModule,
