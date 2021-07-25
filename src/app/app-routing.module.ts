d91c477f02393d7087fc258943e569bdb420ae16
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-07-25 18:24:36 +0300

    updated app component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './navbar/navbar.component';
-import { GithubComponent } from './github/github.component';
 
-const routes: Routes = [
-  { path: 'github', component: GithubComponent},
-
-  { path: '', redirectTo:"/github", pathMatch:"full"},
-];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
