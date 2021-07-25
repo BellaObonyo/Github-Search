fb24054267ab6a394ae7077798a344512c3dcf52
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-07-25 17:22:19 +0300

    updated search.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { GithubComponent } from './github/github.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    GithubComponent
+    GithubComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/search/search.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchFormComponent implements OnInit {
+@Output() emitSearch = new EventEmitter<any>()
+@Output() isRepo= new EventEmitter<any>()
+
+username="";
+
+searchTerm:string;
+
+  constructor() { }
+search(event:any){
+  this.username = event.target.value
+  this.emitSearch.emit(this.username)
+  console.log(this.username)
+}
+  ngOnInit() {
+  }
+
+}
